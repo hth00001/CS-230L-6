f2498bd4eda2d4b32000f6cf2af3963df562cf38
hth00001@mix.wvu.edu <hth00001@mix.wvu.edu>
2023-11-14 17:24:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>CS 230L</h1>
+        <h2>Section - 002</h2>
+        <p>WVU ID: 800360262</p>
+        <p>Hi I am Hunter Harris</p>
+    </div>
+  );
+}
+
+export default App;
